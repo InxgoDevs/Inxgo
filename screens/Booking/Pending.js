78539e7811c9d64e4874c0bdc200be3dbe0a8991
arsal102 <158466408+arsal102@users.noreply.github.com>
2024-03-15 12:32:32 +0500

    pending screen done

--- screens/Booking/Pending.js
+++ screens/Booking/Pending.js
@@ -13,147 +13,17 @@ const Pending = ({ navigation }) => {
   };
 
   return (
-    <View
-      style={{
-        marginTop: "10%",
-        elevation: 10,
-        marginRight: "2%",
-        marginLeft: "2%",
-        backgroundColor: "#FFFFFF",
-        borderRadius: 10,
-      }}
-    >
-      <View style={{ flexDirection: "row", width: "100%", marginTop: 20 }}>
-        <Image style={wallet.image} source={image_upload} />
-        <View style={wallet.LeftContainer}>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={{ fontSize: 18 }}>Mark Tuan</Text>
-          </View>
-          <Text style={{ fontSize: 12 }}>Plumber</Text>
-        </View>
-        <View style={wallet.RightContainer}>
-          <View style={{ flexDirection: "row", alignSelf: "flex-end" }}>
-            <View
-              style={{
-                right: 10,
-                width: 80,
-                height: 30,
-                backgroundColor: "#FFC44D",
-                alignSelf: "center",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text style={{ fontSize: 14, fontWeight: "bold" }}>Accepted</Text>
-            </View>
-          </View>
-          <TouchableOpacity onPress={() => detailJob("Detail")}>
-            <View style={{ flexDirection: "row", alignSelf: "flex-end" }}>
-              <Image
-                style={{ marginTop: 10, width: 15, height: 15 }}
-                source={rightarrow}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
+    <View style={{justifyContent:'center',alignItems:'center',top:50}} >
+      <View>
+        <Image source={require('../../assets/calendar.png')}></Image>
       </View>
-      <View
-        style={{
-          marginTop: 10,
-          marginBottom: 10,
-          width: "90%",
-          flexDirection: "row",
-          alignSelf: "center",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ width: "38%", marginLeft: 5, flexDirection: "row" }}>
-          <Image source={location} />
-          <Text style={{ fontSize: 14, fontWeight: "bold", marginLeft: 2 }}>
-            Sadiq Centre LHR
-          </Text>
-        </View>
-        <View
-          style={{
-            width: "30%",
-            left: 10,
-            flexDirection: "row",
-            alignSelf: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Image style={{ width: 15, height: 15 }} source={hrs} />
-          <Text
-            style={{
-              fontSize: 14,
-              textAlign: "center",
-              fontWeight: "bold",
-              marginLeft: 2,
-            }}
-          >
-            4 hrs
-          </Text>
-        </View>
-        <View
-          style={{
-            width: "30%",
-            alignSelf: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            right: 5,
-            flexDirection: "row",
-          }}
-        >
-          <Image style={{ width: 15, height: 15 }} source={circle} />
-          <Text
-            style={{
-              fontSize: 14,
-              textAlign: "center",
-              fontWeight: "bold",
-              marginLeft: 2,
-            }}
-          >
-            $40/hr
-          </Text>
-        </View>
+      <View style={{marginTop:50}}>
+        <Text>You don’t have any scheduled hiring at this time</Text>
       </View>
-      <View
-        style={{
-          marginBottom: 10,
-          width: "90%",
-          flexDirection: "row",
-          alignSelf: "center",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ width: "50%", marginLeft: 5, flexDirection: "row" }}>
-          <Text style={{ fontSize: 12, marginLeft: 2 }}>Date & Time</Text>
-        </View>
-        <View
-          style={{
-            width: "50%",
-            alignSelf: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            right: 5,
-            flexDirection: "row",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              textAlign: "center",
-              fontWeight: "bold",
-              marginLeft: 2,
-            }}
-          >
-            June 11, 2023 | 11:00 AM
-          </Text>
-        </View>
+      <View style={{marginTop:20}}>
+      <Text style={{fontSize:32,color:"#FFC44D",fontWeight:"600"}}>Hire Now</Text>
       </View>
+      
     </View>
   );
 };
