e3354b66a408861eff54f65c63881db0a80c17bc
Shibbal Farooq <shibbalfarooq66@gmail.com>
2024-03-15 16:08:18 +0500

    Added formik and Yup in sign up screen

--- screens/Login/SignUp.js
+++ screens/Login/SignUp.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
-import { TextInput } from "react-native-paper";
+import React, { useState, useRef } from "react";
+import { TextInput, Button } from "react-native-paper";
 import CheckBox from "@react-native-community/checkbox";
 import * as Yup from "yup";
-
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { Formik } from "formik";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 import appStyle from "../../style/login_start";
 import signUpStyle from "../../style/SignUp";
 import apple from "../../assets/apple.png";
@@ -18,6 +28,9 @@ const openEye = require("../../assets/openeye.png");
 //   passwordLength: Yup.
 //  })
 const SignUp = ({ navigation }) => {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+  const confirmPasswordInput = useRef(null);
   const showToast = () => {
     // Function to show Toast
   };
@@ -49,52 +62,114 @@ const SignUp = ({ navigation }) => {
   };
 
   return (
-    <View style={appStyle.body}>
-      <TouchableOpacity onPress={() => navigation.navigate("StartLogin")}>
-        <Image style={appStyle.arrowbacklogin} source={arrow_back} />
-      </TouchableOpacity>
-      <Toast />
-      <Text style={signUpStyle.welcome}>Create your{"\n"}Account</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      enabled
+      style={{ flex: 1 }}
+    >
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={appStyle.body}>
+          <TouchableOpacity onPress={() => navigation.navigate("StartLogin")}>
+            <Image style={appStyle.arrowbacklogin} source={arrow_back} />
+          </TouchableOpacity>
+          <Toast />
+          <Text style={signUpStyle.welcome}>Create your{"\n"}Account</Text>
+          <View style={styles.container}>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+              }}
+              validationSchema={Yup.object({
+                name: Yup.string().required("Required"),
+                email: Yup.string().email("Invalid Email").required("Required"),
+                password: Yup.string()
+                  .required("Password is required")
+                  .min(6, "Password must be at least 6 characters"),
+                confirmPassword: Yup.string()
+                  .oneOf([Yup.ref("password"), null], "Passwords must match")
+                  .required("Confirm Password is required"),
+              })}
+              onSubmit={(values, formikActions) => {
+                setTimeout(() => {
+                  Alert.alert(JSON.stringify(values));
+                  formikActions.setSubmitting(false);
+                }, 500);
+              }}
+            >
+              {(props) => (
+                <View>
+                  <TextInput
+                    onChangeText={props.handleChange("name")}
+                    onBlur={props.handleBlur("name")}
+                    value={props.values.name}
+                    autoFocus
+                    placeholder="Your Name"
+                    style={styles.input}
+                  />
+                  {props.touched.name && props.errors.name ? (
+                    <Text style={styles.error}>{props.errors.name}</Text>
+                  ) : null}
+                  <TextInput
+                    onChangeText={props.handleChange("email")}
+                    onBlur={props.handleBlur("email")}
+                    value={props.values.email}
+                    placeholder="Email Address"
+                    style={styles.input}
+                    ref={emailInput}
+                  />
+                  {props.touched.email && props.errors.email ? (
+                    <Text style={styles.error}>{props.errors.email}</Text>
+                  ) : null}
+                  <TextInput
+                    onChangeText={props.handleChange("password")}
+                    onBlur={props.handleBlur("password")}
+                    value={props.values.password}
+                    placeholder="Password"
+                    secureTextEntry
+                    style={styles.input}
+                    ref={passwordInput}
+                  />
+                  {props.touched.password && props.errors.password ? (
+                    <Text style={styles.error}>{props.errors.password}</Text>
+                  ) : null}
+                  <TextInput
+                    onChangeText={props.handleChange("confirmPassword")}
+                    onBlur={props.handleBlur("confirmPassword")}
+                    value={props.values.confirmPassword}
+                    placeholder="Confirm Password"
+                    secureTextEntry
+                    style={styles.input}
+                    ref={confirmPasswordInput}
+                  />
+                  {props.touched.confirmPassword &&
+                  props.errors.confirmPassword ? (
+                    <Text style={styles.error}>
+                      {props.errors.confirmPassword}
+                    </Text>
+                  ) : null}
+                  <Button
+                    onPress={props.handleSubmit}
+                    // color="black"
+                    mode="contained"
+                    loading={props.isSubmitting}
+                    disabled={props.isSubmitting}
+                    style={{ marginTop: 16, backgroundColor: "#FFC44D" }}
+                  >
+                    Submit
+                  </Button>
+                </View>
+              )}
+            </Formik>
+          </View>
+          <View style={appStyle.leftContainer}>
+            {/* <CheckBox /> */}
+            {/* <Text style={appStyle.rowLabelText}>Remember Me</Text> */}
+          </View>
 
-      <View style={appStyle.cardContainer}>
-        <TextInput
-          mode="outlined"
-          theme={{
-            colors: { primary: "#FFC44D", underlineColor: "transparent" },
-          }}
-          style={appStyle.inputSearch}
-          onChangeText={(text) => handleState(text, "email")}
-          placeholder="Email"
-        />
-      </View>
-      <View style={appStyle.cardContainer}>
-        <TextInput
-          mode="outlined"
-          theme={{
-            colors: { primary: "#FFC44D", underlineColor: "transparent" },
-          }}
-          style={appStyle.inputSearch}
-          secureTextEntry={!state.passwordVisible} // Toggle secureTextEntry based on password visibility
-          onChangeText={(text) => handleState(text, "password")}
-          placeholder="Password"
-        />
-        <TouchableOpacity
-          style={styles.eyeIconContainer}
-          onPress={togglePasswordVisibility}
-        >
-          <Image
-            source={state.passwordVisible ? openEye : blind} // Toggle the eye icon based on password visibility
-            style={styles.eyeIcon}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <View style={appStyle.leftContainer}>
-        <CheckBox />
-        <Text style={appStyle.rowLabelText}>Remember Me</Text>
-      </View>
-
-      {!state.flag ? (
+          {/* {!state.flag ? (
         <TouchableOpacity
           onPress={() => navigation.navigate("NewPassword")}
           style={appStyle.appButtonContainer}
@@ -103,41 +178,76 @@ const SignUp = ({ navigation }) => {
         </TouchableOpacity>
       ) : (
         <Image style={styles.loadingIcon} source={Loading} />
-      )}
+      )} */}
 
-      <View style={appStyle.cardContainer}>
-        <Text style={signUpStyle.lineText}>or continue with</Text>
-        <Toast ref={(ref) => Toast.setRef(ref)} />
-      </View>
+          <View style={appStyle.cardContainer}>
+            <Text style={signUpStyle.lineText}>or continue with</Text>
+            <Toast ref={(ref) => Toast.setRef(ref)} />
+          </View>
 
-      <View style={appStyle.iContainer}>
-        <TouchableOpacity style={signUpStyle.appButtonSoical}>
-          <Image style={appStyle.google} source={apple} />
-        </TouchableOpacity>
-        <TouchableOpacity style={signUpStyle.appButtonSoical}>
-          <Image style={appStyle.google} source={google} />
-        </TouchableOpacity>
-        <TouchableOpacity style={signUpStyle.appButtonSoical}>
-          <Image style={appStyle.google} source={facebook} />
-        </TouchableOpacity>
-      </View>
+          <View style={appStyle.iContainer}>
+            <TouchableOpacity style={signUpStyle.appButtonSoical}>
+              <Image style={appStyle.google} source={apple} />
+            </TouchableOpacity>
+            <TouchableOpacity style={signUpStyle.appButtonSoical}>
+              <Image style={appStyle.google} source={google} />
+            </TouchableOpacity>
+            <TouchableOpacity style={signUpStyle.appButtonSoical}>
+              <Image style={appStyle.google} source={facebook} />
+            </TouchableOpacity>
+          </View>
 
-      <View style={appStyle.cardContainer}>
-        <Text style={appStyle.signUp}>Already have an Account? </Text>
-        <Text
-          onPress={() => navigation.navigate("Index")}
-          style={Style.TextContainer}
-        >
-          Login
-        </Text>
-      </View>
-    </View>
+          <View style={appStyle.cardContainer}>
+            <Text style={appStyle.signUp}>Already have an Account? </Text>
+            <Text
+              onPress={() => navigation.navigate("Index")}
+              style={styles.TextContainer}
+            >
+              Login
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 export default SignUp;
 
 const styles = StyleSheet.create({
+  TextContainer: {
+    color: "#FFC44D",
+    fontWeight: "bold",
+    fontSize: 15,
+    fontFamily: "Inter",
+    left: 70,
+  },
+  container: {
+    paddingTop: 1,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  title: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  error: {
+    margin: 2,
+    fontSize: 12,
+    color: "red",
+    fontWeight: "bold",
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 8,
+    width: "100%",
+    borderColor: "#ddd",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    marginBottom: 10,
+  },
   eyeIconContainer: {
     position: "absolute",
     right: 10,
@@ -153,15 +263,5 @@ const styles = StyleSheet.create({
     height: 100,
     alignSelf: "center",
     resizeMode: "stretch",
-  },
-});
-
-const Style = StyleSheet.create({
-  TextContainer: {
-    color: "#FFC44D",
-    fontWeight: "bold",
-    fontSize: 15,
-    fontFamily: "Inter",
-    left: 70,
   },
 });
