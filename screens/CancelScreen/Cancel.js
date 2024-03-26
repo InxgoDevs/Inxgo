3798bf6fe46d93857f6959918e099047b954249b
arsal102 <158466408+arsal102@users.noreply.github.com>
2024-03-26 16:35:03 +0500

    package.json updated

--- screens/CancelScreen/Cancel.js
+++ screens/CancelScreen/Cancel.js
@@ -1,111 +1,130 @@
-//import liraries
-import React, { useState} from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image,KeyboardAvoidingView,TextInput } from "react-native";
 import appStyle from "../../style/login_start";
-import { responsiveHeight,responsiveWidth } from "react-native-responsive-dimensions";
+import {
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
 import signUpStyle from "../../style/SignUp";
-import CustomButton from '../../components/CustomButton';
-
+import CustomButton from "../../components/CustomButton";
 import Canceltext from "../../components/CancelText";
-import { TextInput } from "react-native-paper";
+
+import { Platform } from 'react-native';
+
+import {
+  scale,
+  verticalScale,
+  moderateScale,
+  moderateVerticalScale,
+} from "react-native-size-matters";
 
 const arrow_back = require("../../assets/arrow_back.png");
-// create a component
+
 const CancelScreen = ({ navigation }) => {
-    const [selectedOption, setSelectedOption] = useState("first");
+  const [selectedOption, setSelectedOption] = useState("");
+
   const handleRadioSelect = (option) => {
     setSelectedOption(option);
   };
 
   return (
-    
+  
     <View style={styles.container}>
       <View
         style={{
-          height: responsiveHeight(10),
-          marginTop: 30,
+          height: responsiveHeight(7),
+          width: responsiveWidth(100),
           justifyContent: "flex-start",
-          marginLeft: 20,
-          top: 20,
+          alignItems: "center",
           flexDirection: "row",
         }}
       >
         <TouchableOpacity onPress={() => navigation.navigate("Tracker")}>
           <Image style={appStyle.arrow_backF} source={arrow_back} />
         </TouchableOpacity>
-
         <Text style={signUpStyle.welcomeF}>Cancel Task</Text>
       </View>
-      <View style={{ marginLeft: 20 }}>
-        <Text style={{fontSize:13,fontWeight:"300"}}>Please select the reason for cancellation.</Text>
+      <View style={{ height: responsiveHeight(10), justifyContent: "center" }}>
+        <Text style={{ fontSize: 13, fontWeight: "300", marginLeft: 20 }}>
+          Please select the reason for cancellation.
+        </Text>
       </View>
-
-      <Canceltext Cancel="Waiting for long time" />
-     <Canceltext Cancel="Unable to contact supplier" />
-     <Canceltext Cancel="Wrong information shown" />
-     <Canceltext Cancel="The price is not reasonable" />
-    
-   
-     <View style={{marginTop:20,left:20}}>
-     <Text style={{fontSize:16,fontWeight:"500"}}>Others</Text>
-
-     </View>
-     <View>
-     <TextInput
-          mode="outlined"
-          theme={{
-            colors: { primary: "black", underlineColor: "transparent" },
-          }}
-          style={styles.inputSearchs}
-          placeholder="Other Reasons"
+      <View style={{ height: responsiveHeight(45) }}>
+        <Canceltext
+          Cancel="Waiting for long time"
+          selected={selectedOption === "Waiting for long time"}
+          onSelect={() => handleRadioSelect("Waiting for long time")}
         />
-
-     </View>
-     <View>
-        <CustomButton title={"Submit"} color= '#FFC44D'  onPress={()=>navigation.navigate('CancelScreen')}  width={responsiveWidth(80)}/>
+        <Canceltext
+          Cancel="Unable to contact supplier"
+          selected={selectedOption === "Unable to contact supplier"}
+          onSelect={() => handleRadioSelect("Unable to contact supplier")}
+        />
+        <Canceltext
+          Cancel="Wrong information shown"
+          selected={selectedOption === "Wrong information shown"}
+          onSelect={() => handleRadioSelect("Wrong information shown")}
+        />
+        <Canceltext
+          Cancel="The price is not reasonable"
+          selected={selectedOption === "The price is not reasonable"}
+          onSelect={() => handleRadioSelect("The price is not reasonable")}
+        />
       </View>
-
-     
-          
+      <View style={{ height: responsiveHeight(5), justifyContent: "center" }}>
+        <Text style={{ fontSize: 16, fontWeight: "500", marginLeft: 20 }}>
+          Others
+        </Text>
+      </View>
+      <View style={{ height: responsiveHeight(7) }}>
+        <TextInput
+          style={styles.input}
+          placeholder="Other Reasons"
+      
+          textAlignVertical="top"
+        />
+      </View>
+      <View style={{ height: responsiveHeight(14) }} />
+      <View style={{ height: responsiveHeight(9.5) }}>
+        <CustomButton
+          title={"Submit"}
+          color="#FFC44D"
+          onPress={() => navigation.navigate("CancelScreen")}
+          width={responsiveWidth(85)}
+        />
+      </View>
+      
     </View>
+  
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: "#FAFAFA",
-  },
-//   Text: {
-//     borderWidth: 1,
-//     width:responsiveWidth (85),
-//     height: 50,
-//     borderRadius:10,
-//     fontSize: 13,
-//     //textAlign: 'center',
-//     borderColor: "black",
-//     backgroundColor:"#FAFAFA"
-//   },
-inputSearchs: {
-    width: responsiveWidth(85),
-    // left: 10,
-    // textAlign: "right",
-    // marginLeft: 10, // Add left padding to move text slightly to the right
-
-    paddingHorizontal:5,
-    fontSize:13,
     
-    //borderWidth:.5,
-
-    zIndex: 0, // Ensure border has z-index of 0
-   margin: 20,
-   
   },
- 
+  inputSearchs: {
+    width: responsiveWidth(85),
+    paddingHorizontal: 5,
+    fontSize: 13,
+    zIndex: 0,
+    left: 20,
+  },
+  input: {
+    borderWidth:0.5,
+    borderColor: "#E6E6E6",
+    height: responsiveHeight(7),
+    backgroundColor:'#fafafa',
+   // borderRadius: moderateScale(10),
+    paddingLeft: responsiveWidth(1.5),
+    width: responsiveWidth(85),
+    marginLeft:20,
+    fontSize:13,
+    justifyContent:"center"
+    
+  },
 });
 
-//make this component available to the app
 export default CancelScreen;
